4b322472485b880200c02b7b528a935d714f90bd
Ephraim Flores <ephraim.flores@ymail.com>
2025-12-15 13:44:24 -0800

    Setup react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>My Portfolio Site</div>
+    </>
+  )
+}
+
+export default App
